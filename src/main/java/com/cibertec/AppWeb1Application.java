24105802d98262d58ba8ba69e93a3e0eeb6a9f7c
pyquinte <pyquinte@cibertec.edu.pe>
2025-06-26 20:29:33 -0500

    1er Commit

---
+++ src/main/java/com/cibertec/AppWeb1Application.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWeb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWeb1Application.class, args);
+	}
+
+}
